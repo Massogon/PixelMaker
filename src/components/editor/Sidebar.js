d14f3d78744d8e3a4319692732dd4ca3d1508156
MrCows <49926119+MrCows@users.noreply.github.com>
2025-02-05 14:34:33 -0800

    Added importing/exporting

--- src/components/editor/Sidebar.js
+++ src/components/editor/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { exportDataMessagePack, importDataMessagePack } from '../../utils/dataCoder';
 
 function Sidebar({
   currentTool,
@@ -9,7 +10,7 @@ function Sidebar({
   setShowObjectSelector,
   tileDataRef,
   tileImageElements,
-  objectImageElements
+  objectImageElements,
 }) {
   return (
     <div style={{
@@ -50,11 +51,7 @@ function Sidebar({
           <h4>Current Tile</h4>
           <button onClick={() => setShowTileSelector(true)}
             style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-            <img
-              src={tileImageElements[selectedTile].src}
-              alt={selectedTile}
-              style={{ width: 50, height: 50 }}
-            />
+            <img src={tileImageElements[selectedTile].src} alt={selectedTile} style={{ width: 50, height: 50 }} />
           </button>
         </div>
       )}
@@ -63,23 +60,54 @@ function Sidebar({
           <h4>Current Object</h4>
           <button onClick={() => setShowObjectSelector(true)}
             style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-            <img
-              src={objectImageElements[selectedObject].src}
-              alt={selectedObject}
-              style={{ width: 50, height: 50 }}
-            />
+            <img src={objectImageElements[selectedObject].src} alt={selectedObject} style={{ width: 50, height: 50 }} />
           </button>
         </div>
       )}
       <div style={{ marginTop: '10px' }}>
-        <button onClick={() => console.log(tileDataRef.current)}
+        <button
+          onClick={() => exportDataMessagePack(tileDataRef)}
+          style={{
+            backgroundColor: '#444',
+            color: '#fff',
+            border: 'none',
+            padding: '5px 10px',
+            borderRadius: '4px',
+            marginBottom: '5px'
+          }}
+        >
+          Export Data (MsgPack)
+        </button>
+        <br />
+        <input
+          type="file"
+          accept=".msgpack"
+          onChange={(e) => {
+            if (e.target.files.length > 0) {
+              importDataMessagePack(e.target.files[0], tileDataRef);
+            }
+          }}
           style={{
             backgroundColor: '#444',
             color: '#fff',
             border: 'none',
             padding: '5px 10px',
             borderRadius: '4px'
-          }}>
+          }}
+
+        />
+        <br />
+        <button
+          onClick={() => console.log("Tile Data:", tileDataRef.current)}
+          style={{
+            backgroundColor: '#444',
+            color: '#fff',
+            border: 'none',
+            padding: '5px 10px',
+            borderRadius: '4px',
+            marginTop: '5px'
+          }}
+        >
           Print Data Structure
         </button>
       </div>
